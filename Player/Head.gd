096c462638d13e9c5b85a4db87bc900b1bef1555
fearbucker <raopranav@gmail.com>
2023-11-02 10:57:18 -0400

    Improved code readability

--- Player/Head.gd
+++ Player/Head.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-
+#Variables
 @export_node_path("Camera3D") var cam_path := NodePath("Camera")
 @onready var cam: Camera3D = get_node(cam_path)
 @onready var interaction = $Camera/Interaction
@@ -13,14 +13,19 @@ extends Node3D
 @onready var line_helper = $LineHelper
 @onready var line = $LineHelper/Line
 
+# Hook properties
 @export var hook_reach = 25
 @export var grapple_point : NodePath
 @export var mouse_sensitivity := 2.0
 @export var y_limit := 90.0
+
+# Mouse variables
 var mouse_axis := Vector2()
 var rot := Vector3()
+
+#Picked object properties
 var picked_object
-var pull_force = 10
+var pull_force = 20
 
 # Grappling Variables
 @export var max_grapple_speed := 1.5
@@ -28,8 +33,37 @@ var pull_force = 10
 @export var rest_length := 1.0
 var hooked := false
 var grapple_position := Vector3()
-# HOOK STUFF
 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	line.hide()
+	set_hook_reach()
+	mouse_sensitivity = mouse_sensitivity / 1000
+	y_limit = deg_to_rad(y_limit)
+
+# Called when there is an input event
+func _input(event: InputEvent) -> void:
+	# Mouse look (only if the mouse is captured).
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		mouse_axis = event.relative
+		camera_rotation()
+
+# Called every physics tick. 'delta' is constant
+func _physics_process(delta: float) -> void:
+	head_bob()
+	handle_hook()
+	weapon_pivot_visibility()
+	animation_handler()
+	object_handler()
+	
+	#Camera Controls
+	var joystick_axis := Input.get_vector(&"look_left", &"look_right",
+			&"look_down", &"look_up")
+	if joystick_axis != Vector2.ZERO:
+		mouse_axis = joystick_axis * 1000.0 * delta
+		camera_rotation()
+
+# HOOK STUFF
 func set_hook_reach() -> void:
 	hook.scale.y = hook_reach
 
@@ -83,7 +117,6 @@ func draw_hook(length: float) -> void:
 	line.height = length
 	line.position.z = length / -2
 
-	
 func look_for_point() -> void:
 	var grapple_pt := get_node_or_null(grapple_point)
 	if (grapple_pt and hook.is_colliding()) and !hooked:
@@ -92,12 +125,32 @@ func look_for_point() -> void:
 	else:
 		grapple_pt.visible = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	line.hide()
-	set_hook_reach()
-	mouse_sensitivity = mouse_sensitivity / 1000
-	y_limit = deg_to_rad(y_limit)
+# ANIMATION STUFF
+func head_bob():
+	if player.is_on_floor() and player.velocity.length() > 6:
+		camera_animations.play("head_bob")
+	else:
+		camera_animations.play("RESET")
+
+func weapon_pivot_visibility():
+	if picked_object == null:
+		weapon_pivot.visible = true
+	if picked_object != null:
+		weapon_pivot.visible = false
+		
+func animation_handler():
+	if player.is_wall_running == true and not animation_player.current_animation == "attack":
+		animation_player.play("wallrun")
+	elif player.is_wall_running == false and not animation_player.current_animation == "attack":
+		if player.is_sprinting:
+			animation_player.play("sprinting")
+		else:
+			animation_player.play("idle")
+	if Input.is_action_just_pressed("Lclick") and not animation_player.current_animation == "attack":
+		animation_player.stop()
+		animation_player.play("attack")
+
+# OBJECT STUFF
 
 func pick_object():
 	var collider = interaction.get_collider()
@@ -108,58 +161,16 @@ func drop_object():
 	if picked_object != null:
 		picked_object = null
 
-# Called when there is an input event
-func _input(event: InputEvent) -> void:
-	# Mouse look (only if the mouse is captured).
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		mouse_axis = event.relative
-		camera_rotation()
-
-func head_bob():
-	if player.is_on_floor() and player.velocity.length() > 6:
-		camera_animations.play("head_bob")
-	else:
-		camera_animations.play("RESET")
-
-# Called every physics tick. 'delta' is constant
-func _physics_process(delta: float) -> void:
-
-	var joystick_axis := Input.get_vector(&"look_left", &"look_right",
-			&"look_down", &"look_up")
-	head_bob()
-	handle_hook()
-	if picked_object == null:
-		weapon_pivot.visible = true
-	if picked_object != null:
-		weapon_pivot.visible = false
-	
-	if player.is_wall_running == true and not animation_player.current_animation == "attack":
-		animation_player.play("wallrun")
-	elif player.is_wall_running == false and not animation_player.current_animation == "attack":
-		if player.is_sprinting:
-			animation_player.play("sprinting")
-		else:
-			animation_player.play("idle")
-	
-	if Input.is_action_just_pressed("Lclick") and not animation_player.current_animation == "attack":
-		animation_player.stop()
-		animation_player.play("attack")
-	
+func object_handler():
 	if Input.is_action_just_pressed("ePress"):
 		if picked_object == null:
 			pick_object()
 		elif picked_object != null:
 			drop_object()
-
-	if joystick_axis != Vector2.ZERO:
-		mouse_axis = joystick_axis * 1000.0 * delta
-		camera_rotation()
-	
 	if picked_object != null:
 		var a = picked_object.global_transform.origin
 		var b = hand.global_transform.origin
 		picked_object.set_linear_velocity((b-a)*pull_force)
-
 
 func camera_rotation() -> void:
 	# Horizontal mouse look.
